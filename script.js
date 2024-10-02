a9dffe05783694b12a6366a4a4aed82cf23682db
BigG <alphafleetacc@gmail.com>
2024-10-02 21:24:19 +0300

    second push

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function toggleDialog(btn) {
     if (action === "open") dialog.showModal()
     if (action === "close") dialog.close()
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var userAgent = window.navigator.userAgent.toLowerCase(),
+        safari = /safari/.test(userAgent),
+        ios = /iphone|ipod|ipad/.test(userAgent);
+  
+    if(ios) {
+        if (!safari) {
+            // ios webview
+            document.getElementById("header").style.display = "none";
+            document.getElementById("nav-toggle").style.display = "none";
+            document.getElementById("footer").style.display = "none";
+        };
+    };
+}, false);
